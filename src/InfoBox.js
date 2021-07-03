28c72a5fd417de81f6997820ec27167672033640
yashasoni <yashasoni4@gmail.com>
2021-07-03 15:17:49 +0530

    git commit

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,30 +1,30 @@
-import React from "react";
-import { Card, CardContent, Typography } from "@material-ui/core";
+//rfce(react functional compo with an export)
+import React from 'react';
+import {Card, CardContent, Typography} from '@material-ui/core';
 import "./InfoBox.css";
+function InfoBox({title, cases, total, isRed, active, ...props}) {
+    return (
+        <Card 
+            onClick={props.onClick} 
+            className={`infoBox ${active && "infoBox--selected"} ${isRed && "infoBox--red"}`}
+        >
+            <CardContent>
+                {/* Title i.e. Coronavirus cases */}
+                <Typography className="infoBox__title" color="textSecondary">
+                    {title}
+                </Typography>
+                
+                {/* +120k Number of cases */}
+                <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>{cases}</h2>
 
-function InfoBox({ title, cases, total, active, isRed, ...props }) {
-  console.log(title, active);
-  return (
-    <Card
-      onClick={props.onClick}
-      className={`infoBox ${active && "infoBox--selected"} ${
-        isRed && "infoBox--red"
-      }`}
-    >
-      <CardContent>
-        <Typography color="textSecondary" gutterBottom>
-          {title}
-        </Typography>
-        <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>
-          {cases}
-        </h2>
-
-        <Typography className="infoBox__total" color="textSecondary">
-          {total} Total
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+                {/* 1.2M Total */}
+                <Typography className="infoBox__total" color="textSecondary">
+                    {total} Total
+                </Typography>
+            </CardContent>
+            
+        </Card>
+    )
 }
 
 export default InfoBox;
--- src/Map.js
+++ src/Map.js
@@ -1,20 +1,27 @@
-import React from "react";
-import { Map as LeafletMap, TileLayer } from "react-leaflet";
+import React from 'react'
 import "./Map.css";
-import { showDataOnMap } from "./util";
-
-function Map({ countries, casesType, center, zoom }) {
-  return (
-    <div className="map">
-      <LeafletMap center={center} zoom={zoom}>
+import ChangeView from './ChangeView';
+import {MapContainer, TileLayer} from "react-leaflet";
+import {showDataOnMap} from "./util";
+function Map({countries, casesType, center, zoom}) {
+    return (
+        <div className="map">
+        <MapContainer
+        className="markercluster-map"
+        center={center}
+        zoom={zoom}
+      >
+        <ChangeView center = {center} zoom={zoom} />
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {showDataOnMap(countries, casesType)}
-      </LeafletMap>
-    </div>
-  );
+      </MapContainer>
+        
+      
+        </div>
+    )
 }
 
-export default Map;
+export default Map
